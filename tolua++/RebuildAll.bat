f5c0da3c06637017054f693984c6cd84dd6cbead
David Joffe <david@djsoftware.tech>
2026-04-22 21:31:09 +0200

    RebuildAll bat helper for commandline builds of this

---
+++ tolua++/RebuildAll.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+set "PROJECT=%~dp0tolua++.vcxproj"
+
+set "MSBUILD=MSBuild.exe"
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" (
+    for /f "usebackq tokens=*" %%I in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+        set "MSBUILD=%%I"
+    )
+)
+
+call :Rebuild Debug Win32 || exit /b 1
+call :Rebuild Debug x64 || exit /b 1
+call :Rebuild Release Win32 || exit /b 1
+call :Rebuild Release x64 || exit /b 1
+
+echo.
+echo Rebuilt tolua++ static libraries:
+if exist "%~dp0Debug\tolua++D-x86.lib" echo   Debug Win32:   "%~dp0Debug\tolua++D-x86.lib"
+if exist "%~dp0x64\Debug\tolua++D-x64.lib" echo   Debug x64:     "%~dp0x64\Debug\tolua++D-x64.lib"
+if exist "%~dp0Release\tolua++-x86.lib" echo   Release Win32: "%~dp0Release\tolua++-x86.lib"
+if exist "%~dp0x64\Release\tolua++-x64.lib" echo   Release x64:   "%~dp0x64\Release\tolua++-x64.lib"
+
+exit /b 0
+
+:Rebuild
+echo.
+echo Rebuilding tolua++ %~1 %~2...
+"%MSBUILD%" "%PROJECT%" /m /t:Rebuild /p:Configuration=%~1 /p:Platform=%~2
+exit /b %ERRORLEVEL%
